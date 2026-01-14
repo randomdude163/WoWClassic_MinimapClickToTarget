e40b31c34d49de348e5ed09f492f56e90d7413cf
randomdude <randomdude163@posteo.org>
2026-01-14 14:25:04 +0100

    zip addon script

--- zip_addon.sh
+++ zip_addon.sh
@@ -1,10 +1,12 @@
 addon_name="MinimapClickToTarget"
-rm $addon_name.zip
+
+rm ${addon_name}.zip
 mkdir _zip
-cp -r $addon_name _zip
+cp -r ${addon_name} _zip
 cd _zip
+rm -r ${addon_name}/img/Screenshots
 find . -name "*.DS_Store" -type f -delete
-zip -r $addon_name.zip $addon_name
-mv $addon_name.zip ..
+zip -r ${addon_name}.zip ${addon_name}
+mv ${addon_name}.zip ..
 cd ..
 rm -r _zip
